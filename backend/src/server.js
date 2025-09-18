9e60762eafbe83bfaf1cddc6a13b79ebc408abf2
Maisasul <maisasul8@gmail.com>
2025-09-18 09:22:57 +0400

    add mongoose and setup MongoBD connection file

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,13 @@
 import express from "express";
+import { connectDB } from "./config/db.js";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
+
+connectDB();
 
 app.get('/api/notes', (req, res) => {
   res.send("message");
