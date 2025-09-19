4647c039247a4e29b8b79daff23c5ca4682ace9f
Maisasul <maisasul8@gmail.com>
2025-09-19 18:03:56 +0400

    chore: add CORS middleware, root route and 404 handler

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
 import dotenv from 'dotenv';
+import cors from 'cors';
 import projectRoutes from './routes/projectRoutes.js';
-import tasksRouter from './routes/tasksRoutes.js';
+import tasksRoutes from './routes/tasksRoutes.js';
 
 dotenv.config();
 
@@ -12,11 +13,19 @@ const PORT = process.env.PORT || 5001;
 connectDB();
 
 //middlewera 
+app.use(cors());
 app.use(express.json());
 
-app.use('/api/projects', projectRoutes);
+app.get('/', (req, res) => {
+  res.send('API is running..');
+});
 
-app.use('/api/tasks', tasksRouter);
+app.use('/api/projects', projectRoutes);
+app.use('/api/tasks', tasksRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({message: 'Route not found'});
+});
 
 app.listen(PORT, () => {
   console.log(`server running on http://localhost:${PORT}`);
